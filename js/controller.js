ddb11f6d6bc26c32062bc426690354f15541c4c5
Nicolas <morales.nicolas1996wosmats@gmail.com>
2023-03-04 03:02:40 -0500

    [add] Nav con Banner y orden de las carpetas

---
+++ js/controller.js
@@ -0,0 +1,12 @@
+
+    $(function() {
+        $("#nav").load("nav.html");
+     });
+
+     function abrirAbout() {
+        $("#parent").load("about.html");
+     }
+
+     function abrirContact() {
+        $("#parent").load("contact.html");
+     }
